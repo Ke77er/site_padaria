4f25aa9f692e352bfafc18e1fda0db34619093b7
Keller <Jonatan.keller@hotmail.com>
2024-03-13 09:38:54 -0300

    first commit

---
+++ src/components/section_about.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="section-about" class="section-about">
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SectionAbout",
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  mounted() {
+  },
+  methods: {
+  },
+};
+</script>
+<style>
+@import url('../src/assets/styles.css');
+</style>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/section_portifolio.vue
@@ -0,0 +1,24 @@
+<template>
+  <section id="section-portifolio" class="section-portifolio">
+  
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SectionPortifolio",
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+}
+</script>
+<style>
+@import url('../src/assets/styles.css');
+</style>
+
+<style scoped>
+
+</style>
